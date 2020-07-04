5a833b3faa08194ce0dd122ea82b55ffdae03d0a
DG-Develop <dgdband@hotmail.com>
2020-07-04 13:38:43 -0500

    Ad Mob

---
+++ app/src/main/java/com/dgdevelop/admob/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.dgdevelop.admob
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this) {}
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
+
+    }
+}
